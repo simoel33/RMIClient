f5d663199866a19e40ffb962b7349de1b0be4420
Med-akraou <medakraou21@gmail.com>
2021-12-24 15:09:11 +0100

    jdjdj

--- src/app/Client.java
+++ src/app/Client.java
@@ -4,14 +4,18 @@ import java.rmi.Naming;
 
 import beans.Livre;
 import beans.User;
-import db.UserDb;
+
 
 public class Client {
 	public static void main(String[] args) {
 		 try {
 	            IBiblio stub= (IBiblio) Naming.lookup("rmi://localhost:1099/BK");
-	            User u= stub.findUserById("P1234");
+	            User u= (User)stub.findUserById("P1234");
+
 	            System.out.println(u.getAdress());
+	            Livre l=(Livre)stub.findBookById(1233);
+	            stub.Emprunter(l, u, "2021-12-31");
+	           
 	            
 	           
 	        } catch (Exception e) {
